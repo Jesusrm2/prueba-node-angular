d041ba3802781ee594bb2b8ebea63a8175cf973e
Jesusrm2 <135676762+Jesusrm2@users.noreply.github.com>
2024-06-11 15:24:39 -0500

    Update store-procedure-crear-admin.sql

--- store-procedure-crear-admin.sql
+++ store-procedure-crear-admin.sql
@@ -7,7 +7,7 @@ DECLARE
 BEGIN
     INSERT INTO "public"."roles" ("rolid", "rolname") VALUES (1, 'administrador'), (2, 'usuario');
     INSERT INTO "public"."personas" ("personaid", "nombres", "apellidos", "identificacion", "fecha_nacimiento")
-    VALUES (1, 'Admin', 'Admin', '1234567890', CURRENT_TIMESTAMP);
+    VALUES (1, 'Admin', 'Admin', '1234567890', '2024-06-11 00:00:00');
     SELECT "rolid" INTO admin_role_id FROM "public"."roles" WHERE "rolname" = 'administrador';
     SELECT "personaid" INTO admin_person_id FROM "public"."personas" WHERE "identificacion" = '1234567890';
 
